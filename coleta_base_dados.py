7b528394315946adf2fd8c82572e0d08f032b2c3
Filipe Ribeiro <estagiario_feliper@ciasc.sc.gov.br>
2024-05-14 17:40:10 -0300

    Ajuste na coleta

--- coleta_base_dados.py
+++ coleta_base_dados.py
@@ -1,56 +1,90 @@
 import requests
 import pandas as pd
 import time
+from bs4 import BeautifulSoup
+import re
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-# https://onlinejudge.u-aizu.ac.jp/solutions/search
+url = 'https://onlinejudge.u-aizu.ac.jp/solutions/search'
+
+# Simulador de browser
+service = Service(ChromeDriverManager().install())
+
+chrome = webdriver.ChromeOptions()
+chrome.add_argument('--headless')
+chrome.add_argument('--no-sandbox')
+chrome.add_argument('--disable-dev-shm-usage')
+
+browser = webdriver.Chrome(service=service, options=chrome)
+browser.get(url)
+
+site = BeautifulSoup(browser.page_source, "html.parser")
+
+lista_nomes = site.find_all('div', {'class':'info'})
+print(lista_nomes)
+
+browser.quit()
 
 df = pd.DataFrame(columns=['judgeId','problemId','sourceCode'])
 
+judgeId = []
+# for i in range(1,9996):
+#     print('Problema', i)
+#     j = str(i).zfill(4)
+    
+#     response = requests.get(f"https://judgeapi.u-aizu.ac.jp/solutions/problems/{j}/lang/python3")
+#     dados = response.json()
+#     for dado in dados:
+#         politica = dado["policy"]
+#         if politica == "public":
+#             judgeId.append(dado["judgeId"])
+    
+#     # response = requests.get(f"https://judgeapi.u-aizu.ac.jp/solutions/problems/ALDS1_{i}_A/lang/python3")
+#     # dados = response.json()
+#     # for dado in dados:
+#     #     politica = dado["policy"]
+#     #     if politica == "public":
+#     #         judgeId.append(dado["judgeId"])
 
-for i in range(3,16):
-    judgeId = []
-    response = requests.get(f"https://judgeapi.u-aizu.ac.jp/solutions/problems/ALDS1_{i}_A/lang/python3")
-    dados = response.json()
-    for dado in dados:
-        politica = dado["policy"]
-        if politica == "public":
-            judgeId.append(dado["judgeId"])
+#     # response = requests.get(f"https://judgeapi.u-aizu.ac.jp/solutions/problems/ALDS1_{i}_B/lang/python3")
+#     # dados = response.json()
+#     # for dado in dados:
+#     #     politica = dado["policy"]
+#     #     if politica == "public":
+#     #         judgeId.append(dado["judgeId"])
 
-    response = requests.get(f"https://judgeapi.u-aizu.ac.jp/solutions/problems/ALDS1_{i}_B/lang/python3")
-    dados = response.json()
-    for dado in dados:
-        politica = dado["policy"]
-        if politica == "public":
-            judgeId.append(dado["judgeId"])
+#     # response = requests.get(f"https://judgeapi.u-aizu.ac.jp/solutions/problems/ALDS1_{i}_C/lang/python3")
+#     # dados = response.json()
+#     # for dado in dados:
+#     #     politica = dado["policy"]
+#     #     if politica == "public":
+#     #         judgeId.append(dado["judgeId"])
 
-    response = requests.get(f"https://judgeapi.u-aizu.ac.jp/solutions/problems/ALDS1_{i}_C/lang/python3")
-    dados = response.json()
-    for dado in dados:
-        politica = dado["policy"]
-        if politica == "public":
-            judgeId.append(dado["judgeId"])
+#     # response = requests.get(f"https://judgeapi.u-aizu.ac.jp/solutions/problems/ALDS1_{i}_D/lang/python3")
+#     # dados = response.json()
+#     # for dado in dados:
+#     #     politica = dado["policy"]
+#     #     if politica == "public":
+#     #         judgeId.append(dado["judgeId"])
 
-    response = requests.get(f"https://judgeapi.u-aizu.ac.jp/solutions/problems/ALDS1_{i}_D/lang/python3")
-    dados = response.json()
-    for dado in dados:
-        politica = dado["policy"]
-        if politica == "public":
-            judgeId.append(dado["judgeId"])
-
-    for id in judgeId:
-        try:
-            response = requests.get(f"https://judgeapi.u-aizu.ac.jp/reviews/{id}")
-            dado = response.json()
-            id_judge = dado["judgeId"]
-            id_problem = dado["problemId"]
-            codigo = dado["sourceCode"]
-        except requests.exceptions.ConnectionError:
-            time.sleep(15)
-            continue
-        # print(id)
+# print(len(judgeId))
+# for id in judgeId:
+    
+#     try:
+#         response = requests.get(f"https://judgeapi.u-aizu.ac.jp/reviews/{id}")
+#         dado = response.json()
+#         id_judge = dado["judgeId"]
+#         id_problem = dado["problemId"]
+#         codigo = dado["sourceCode"]
+#         time.sleep(5)
+#     except requests.exceptions.ConnectionError:
+#         time.sleep(5)
+#         # print(id)
         
-
-        df_novo = pd.DataFrame([[id_judge,id_problem,codigo]], columns=df.columns)
-        df = pd.concat([df,df_novo], ignore_index=True)
-    df.to_csv(f'{i}_data_frame_python.csv', index=False)
-    print(df)
+#     df_novo = pd.DataFrame([[id_judge,id_problem,codigo]], columns=df.columns)
+#     df = pd.concat([df,df_novo], ignore_index=True)
+#     print(df)
+# df.to_csv('data_frame_python.csv', index=False)
+# print(df)
